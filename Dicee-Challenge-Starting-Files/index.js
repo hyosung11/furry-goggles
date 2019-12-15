2709c8a9fc04e010fe43ac14f825d069e007bc0e
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-15 14:43:01 -0500

    finished dice game

--- Dicee-Challenge-Starting-Files/index.js
+++ Dicee-Challenge-Starting-Files/index.js
@@ -18,3 +18,17 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 let randomImageSource2 = "images/dice" + randomNumber2 + ".png"
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// Messages
+
+// If Player 1 Wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+// If Player 2 Wins
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+// Tie 
+} else {
+  document.querySelector("h1").innerHTML = "It's a tie. Roll again."
+}
