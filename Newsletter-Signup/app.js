23e29b7edd28b1c076c46ee255bbb9e3974df507
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-17 12:39:56 -0500

    update app.js

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,13 +1,15 @@
 // dependencies
 const bodyParser = require('body-parser');
 const express = require('express');
-const request = require('request')
+const request = require('request');
+require('dotenv').config();
+
+// console.log(process.env.API_KEY);
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-
 
 
 // ROUTES
@@ -20,11 +22,47 @@ app.get('/', function(req, res) {
 // post route
 app.post('/', function(req, res) {
 
-  let firstName = req.body.firstName;
-  let lastName = req.body.lastName;
-  let email = req.body.email;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const apiKey = process.env.API_KEY;
 
-  console.log(firstName, lastName, email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed',
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const options = {
+    url: 'https://us4.api.mailchimp.com/3.0/lists/3d26701bea',
+    method: 'POST',
+    headers: {
+      'Authorization': 'hyosung11 apiKey'
+    },
+    body: jsonData
+  };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      res.send("There was an error with signing up, please try again!")
+    } else {
+      if (response.statusCode === 200) {
+        res.send("Successfully subscribed!")
+      } else {
+        res.send("There was an error with signing up, please try again!")
+      }
+    }
+  });
 
 });
 
@@ -32,7 +70,3 @@ app.post('/', function(req, res) {
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
-
-
-// MailChimp API Key 
-// f3a6e100fc80643fb137e29c13e7693a-us4
