129bd5dc651637fd410ab4009329bc3c1028593d
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-16 20:34:36 -0500

    start newsletter signup app

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,34 @@
+// dependencies
+const bodyParser = require('body-parser');
+const express = require('express');
+const request = require('request')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+
+// ROUTES
+
+// get route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+// post route
+app.post('/', function(req, res) {
+
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+});
+
+// listener
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
