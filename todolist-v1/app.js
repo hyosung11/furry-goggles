f7c690be2481ad6d3607fa4cf9531501e76328df
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-17 17:56:07 -0500

    render new list items

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,47 +5,42 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // get route
 
 app.get('/', (req, res) => {
 
   const today = new Date();
-  const currentDay = today.getDay();
-  let day = '';
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
-    default:
-    console.log('Error: current day is equal to: ' + currentDay);
-  }
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
 
-    res.render('list', {
-      kindOfDay: day
-    });
+  const day = today.toLocaleDateString('en-US', options);
+
+  res.render('list', {kindOfDay: day, newListItems: items});
 
 });
+
+// post route
+
+app.post('/', (req, res) => {
+  item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect('/');
+
+});
+
+
 
 // listener
 app.listen(3000, () => {
